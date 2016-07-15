2e800fa854d43db5bf7b04f8267b0ee3010fdfc6
Daniel Weflen <danweflen@gmail.com>
2016-07-15 14:03:46 -0600

    Nonworking adaptation of h_sub to slurminator.
    Slurminator has been almost totally rewritten, but
    hasn't been tested yet.

--- slurminator.py
+++ slurminator.py
@@ -4,58 +4,55 @@ from itertools import product
 from subprocess import STDOUT,PIPE,Popen,call
 import math
 import sys
+import uuid
 
-queue="batch"
-key=0
-processors=1
-config_prefix="/home/becker/weflen/Queue_Config/"
-prog_name="/home/becker/weflen/npsf_h/npsf_h"
-queue_prefix=open("queue_h_prefix","r").read()
-run_name="second_trial_run"
-output_dir="/mpdata/becker/weflen/H/"+run_name+"/"
-jobname=output_dir+run_name
-queue_display_name=run_name
-
-cep_pts=20
-parameter_lists={"n_points":[(750,750)],
-                 "dx":[(0.2,0.2)],
-                 "dt":[0.015],
-                 "EnergyThreshold":[1.0E-13],
-                 "outputFolder":["\""+output_dir+"\""],
-                 "wavefunctionFolder":["\""+output_dir+"\""],
-                 "run_name":["\""+run_name+"\""],
-                 "wavelength_nm":[800],
-                 "intensity_w_cm2":[1E13,5E13,1E14],
-                 "carrier_envelope_phase":[(2.0*math.pi/cep_pts)*x for x in range(0,cep_pts)],
-                 "pulse_length_cycles":[3,5,10],
-                 "free_propagation_time":[500],
-                 "pre_run_propagation_time":[1000],
-                 "soft_core":[0.5], 
-                 "absorber_fraction":[tuple(0.2 for x in range(4))],
-                 "Z_eff":[1.9],
-                 "jobname":["\""+jobname+"\""]}
-
-param_dicts=[zip(parameter_lists.keys(), params) for params in product(*parameter_lists.values())]
-
-
-def print_list_as_config_file(filename, params,key):
+# A "config file" in this sense is a short string of valid matlab assignments. You
+#can use it to get 
+def print_list_as_config_file(filename, params,identifier):
    with open(filename,"w") as config_file:
       output_string=""
       for item in params:
-         output_string=output_string+str(item[0])+"="+str(item[1])+"\n"
-      config_file.write(output_string+"key="+str(key)+"\n")
-      
+         output_string=output_string+str(item[0])+"="+str(item[1])+";\n"
+      config_file.write(output_string+"identifier="+str(identifier)+"\n")
 
-for parameter_dict in param_dicts:
-   print_list_as_config_file(config_prefix+"config"+str(key)+".inp",parameter_dict,key)
-   queue_file_text=queue_prefix+"\n#PBS -l nodes=1:ppn="+str(processors)+"\n\n"
-   queue_file_text=queue_file_text+"#PBS -N "+queue_display_name+"\n\n"
-   queue_file_text=queue_file_text+"#PBS -o "+prog_name"_"run_name+".out\n\n"
-   queue_file_text=queue_file_text+"\n"+"mkdir -p "+output_dir+"\n"
-   queue_file_text=queue_file_text+"\n"+"export OMP_NUM_THREADS="+str(processors)+"\n\n\n"
-   queue_file_text=queue_file_text+prog_name+" "+config_prefix+"config"+str(key)+".inp\n"
-   open("queue_h.bash","w").write(queue_file_text)
-   result_string=Popen(["qsub","-q",queue, "queue_h.bash"],stderr=STDOUT,stdout=PIPE).stdout.read().decode("utf-8")
-   sys.stdout.write(result_string)
-   key=key+1
+def slurminator(variable_filename):
+   varfile=open(variable_filename)
+   #Evaluates the first 10 megabytes of a python script. If you're configuration file
+   #is longer than 10 megabytes, you're doing something wrong. Also, "eval" as used
+   #here is probably not a problem, as this script is only useful to people who
+   #have permission to run arbitrary code on the machine anyway.
+   eval(varfile.read(int(1E7)))
+   #The file denoted by variable_filename needs to define the following variables,
+   #in standard python syntax. You can define whatever you want, or do
+   #arbitrary computation as well:
+   #  queue: The name of the queue to which the job will be submitted.
+   #  processors: The number of processors needed.
+   #  memory: The amount of memory needed. A string with a number and unit, like "6GB".
+   #  standard_output: File name to dump stdout and stderr. Important for debugging.
+   #  job_name: The name that the job will appear as in the queue.
+   #  parameter_lists: A dictionary of lists. The job will be run for every value of every list.
+   #    Parameter lists will be turned into a series of matlab assignments, to be executed in matlab
+   #    before the main script. This allows you to have seperate jobs for each subject (for
+   #    preprocessing) or region (for group-level analysis), taking full advantage of the parallelism
+   #    of the machine you're running on.
+
+   matlab_script_name="example.m"
+   #This converts a dictionary of lists to a list of dictionaries. Each dictionary is a
+   #representation of a single job.
+   param_dicts=[zip(parameter_lists.keys(), params) for params in product(*parameter_lists.values())]
+   for parameter_dict in param_dicts:
+      job_id=uuid.uuid4() #Generates a random, universally unique identifier.
+      print_list_as_config_file(config_prefix+"config"+str(job_id)+".inp",parameter_dict,job_id)
+      queue_file_text=queue_prefix+"\n#PBS -l nodes=1:ppn="+str(processors)+"\n\n"
+      queue_file_text=queue_file_text+"#PBS -N "+queue_display_name+"\n\n"
+      queue_file_text=queue_file_text+"#PBS -o "+prog_name"_"run_name+".out\n\n"
+      queue_file_text=queue_file_text+"\n"+"mkdir -p "+output_dir+"\n"
+      queue_file_text=queue_file_text+"\n"+"export OMP_NUM_THREADS="+str(processors)+"\n\n\n"
+      queue_file_text=queue_file_text+prog_name+" "+config_prefix+"config"+str(job_id)+".inp\n"
+      open("queue_h.","w").write(queue_file_text)
+      result_string=Popen(["srun","-q",queue, "queue_h.bash"],stderr=STDOUT,stdout=PIPE).stdout.read().decode("utf-8")
+      sys.stdout.write(result_string)
+
+if __name__ =="__main__":
+   slurminator(sys.argv[1])
 
